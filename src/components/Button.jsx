6f86e94798504069bef05006119a2c49e29cc7a3
Vinoth Subramanian <157690987+Vino-Siva@users.noreply.github.com>
2024-10-17 19:21:39 +0530

    add glow effect and other ui classes

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -13,8 +13,8 @@ const Button = ({
 }) => {
   const Inner = () => (
     <>
-      <span className="">
-        <span>
+      <span className="relative flex items-center min-h-[60px] px-4 g4 rounded-2xl inner-before group-hover:before:opacity-100 overflow-hidden">
+        <span className="absolute -left-[1px]">
           <Marker fill={markerFill} />
         </span>
         {icon && (
@@ -24,8 +24,11 @@ const Button = ({
             alt="circle"
           />
         )}
-        <span>{children}</span>
+        <span className="relative z-2 font-poppins base-bold text-p1 uppercase">
+          {children}
+        </span>
       </span>
+      <span className="glow-before glow-after" />
     </>
   );
 
@@ -33,7 +36,7 @@ const Button = ({
     <a
       href={href}
       className={clsx(
-        "relative, p-0.5 g5 rounded-2xl shadow-500 group",
+        "relative p-0.5 g5 rounded-2xl shadow-500 group",
         containerClassName
       )}
     >
@@ -42,7 +45,7 @@ const Button = ({
   ) : (
     <button
       className={clsx(
-        "relative, p-0.5 g5 rounded-2xl shadow-500 group",
+        "relative p-0.5 g5 rounded-2xl shadow-500 group",
         containerClassName
       )}
       onClick={onClick}
